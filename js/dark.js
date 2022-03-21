94de4d7e98bbfe3e5ff8cd233382a1fffebb544f
Louis <vachemeuuh@gmail.com>
2022-03-21 16:30:26 +0100

    Dark mode

---
+++ js/dark.js
@@ -0,0 +1,48 @@
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+  document.getElementById("inputSection").classList.toggle("dark-theme");
+  document.getElementById("gamesPlayed").classList.toggle("dark-theme");
+  document.getElementById("difficulties").classList.toggle("dark-theme");
+  document.getElementById("lifePlaytime").classList.toggle("dark-theme");
+  document.getElementById("averagePlaytime").classList.toggle("dark-theme");
+  document.getElementById("mouseClicks").classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+  document.getElementById("inputSection").classList.toggle("light-theme");
+  document.getElementById("gamesPlayed").classList.toggle("light-theme");
+  document.getElementById("difficulties").classList.toggle("light-theme");
+  document.getElementById("lifePlaytime").classList.toggle("light-theme");
+  document.getElementById("averagePlaytime").classList.toggle("light-theme");
+  document.getElementById("mouseClicks").classList.toggle("light-theme");
+}
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    document.getElementById("inputSection").classList.toggle("light-theme");
+    document.getElementById("gamesPlayed").classList.toggle("light-theme");
+    document.getElementById("difficulties").classList.toggle("light-theme");
+    document.getElementById("lifePlaytime").classList.toggle("light-theme");
+    document.getElementById("averagePlaytime").classList.toggle("light-theme");
+    document.getElementById("mouseClicks").classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    document.getElementById("inputSection").classList.toggle("dark-theme");
+    document.getElementById("gamesPlayed").classList.toggle("dark-theme");
+    document.getElementById("difficulties").classList.toggle("dark-theme");
+    document.getElementById("lifePlaytime").classList.toggle("dark-theme");
+    document.getElementById("averagePlaytime").classList.toggle("dark-theme");
+    document.getElementById("mouseClicks").classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
